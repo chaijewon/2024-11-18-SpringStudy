83e599cc4eae3e7cf1d0b11ba0796af9786bc4b0
sist <sist@sist-101>
2025-03-20 10:53:11 +0900

    11111

--- SpringDIProject_9(XML+Annotation)/src/main/java/com/sist/main/MainClass.java
+++ SpringDIProject_9(XML+Annotation)/src/main/java/com/sist/main/MainClass.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.sist.dao.*;
 import com.sist.vo.*;
 public class MainClass {
-
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         ApplicationContext app=
