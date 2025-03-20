14ac7667fc7968f0213fdbbd15d3ffe66b341796
sist <sist@sist-101>
2025-03-20 12:51:20 +0900

    1111

---
+++ SpringDIProject_11(total_mybatis_parameter)/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,33 @@
+package com.sist.main;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.sist.service.FoodService;
+import java.util.*;
+import com.sist.vo.*;
+@Component
+public class MainClass {
+	@Autowired
+    private FoodService service;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ApplicationContext app=
+        		new ClassPathXmlApplicationContext("application-*.xml");
+        MainClass mc=(MainClass)app.getBean("mainClass");
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Page ют╥б:");
+        int page=scan.nextInt();
+        List<FoodVO> list=
+        	mc.service.foodListData((page*10)-9, page*10);
+        for(FoodVO vo:list)
+        {
+        	System.out.println(vo.getFno()+"."
+        			+vo.getName());
+        }
+	}
+
+}
