70e26d0f2b74556cf988ce8e7d3676ba1f37e30f
sist <sist@sist-101>
2025-03-26 12:50:33 +0900

    1111

--- SpringWebProject_4/src/main/java/com/sist/vo/FoodVO.java
+++ SpringWebProject_4/src/main/java/com/sist/vo/FoodVO.java
@@ -29,5 +29,5 @@ public class FoodVO {
    private int fno,hit,jjimcount,likecount,replycount;
    private double score;
    private String name,type,theme,poster,images,time,
-           parking,content,price,rdays;
+           parking,content,price,rdays,address,phone;
 }
