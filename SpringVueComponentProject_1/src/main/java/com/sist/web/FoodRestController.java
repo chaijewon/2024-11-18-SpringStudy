8e70cd29d226c9f9a1e9c1dd2b8da2ed4c588063
sist <sist@sist-101>
2025-04-11 16:33:10 +0900

    1111

--- SpringVueComponentProject_1/src/main/java/com/sist/web/FoodRestController.java
+++ SpringVueComponentProject_1/src/main/java/com/sist/web/FoodRestController.java
@@ -1,5 +1,17 @@
 package com.sist.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+@RestController
 public class FoodRestController {
-
+  @PostMapping("food/vue_check2_vue.do")
+  public Map vue_check(String ss,String[] f)
+  {
+	  System.out.println("ss:"+ss);
+	  Map map=new HashMap();
+	  map.put("f", f);
+	  return map;
+  }
 }
