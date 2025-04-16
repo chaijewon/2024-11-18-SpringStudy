75e189809e2a7fb47c9fa4a9024a39acf61520cc
sist <sist@sist-101>
2025-04-16 10:50:26 +0900

    1111

---
+++ SpringMyBatisProcedureProject_1/src/main/java/com/sist/web/FoodController.java
@@ -0,0 +1,22 @@
+package com.sist.web;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sist.dao.*;
+import com.sist.vo.*;
+@Controller
+public class FoodController {
+    @Autowired
+    private FoodDAO dao;
+    
+    @GetMapping("food/list.do")
+    public String food_list(String page,Model model)
+    {
+    	
+    	return "food/list";
+    }
+}
